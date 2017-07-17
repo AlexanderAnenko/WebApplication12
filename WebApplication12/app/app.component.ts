6a8e2e81f32dd0cbe939217b4db45fd3a4fa7411
Аненко Олександр <alex710@ua.fm>
2017-07-17 18:25:40 +0300

    Test

--- WebApplication12/app/app.component.ts
+++ WebApplication12/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'my-app',
-  template: '<h1>Hello World</h1>'
+    selector: 'my-app',
+    template: `<label>Введите имя:</label>
+                 <input [(ngModel)]="name" placeholder="name">
+                 <h1>Добро пожаловать: {{name}}!</h1>`
 })
-export class AppComponent { }
+
+export class AppComponent {
+    name = '';
+}
+
+
+//Запись изменений
